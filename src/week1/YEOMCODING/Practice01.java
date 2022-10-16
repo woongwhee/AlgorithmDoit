b71fcb16f74ec7b7666b15d1aa7af701121a3c44
YEOMCODING <duatjgkr123@gmail.com>
2022-10-16 15:47:06 +0900

    YeomCommit

---
+++ src/week1/YEOMCODING/Practice01.java
@@ -0,0 +1,292 @@
+package seo.hak.station01;
+
+import java.util.Scanner;
+
+public class Practice01 {
+	
+	public static int Max4(int a, int b, int c, int d) {
+		
+		int max = a;
+		if(b > max) {
+			max = b;
+		}
+		if(c > max) {
+			max = c;
+		}
+		if(d > max) {
+			max = d;
+		}
+		
+		return max;
+	}
+	
+	public static int Min3(int a, int b , int c) {
+		
+		int min = a;
+		if(b < min) {
+			min = b;
+		}
+		if (c < min) {
+			min = c;
+		}
+		return min;
+	}
+
+	public static int Min4(int a, int b, int c, int d) {
+
+		int min = a;
+		if (b < min) {
+			min = b;
+		}
+		if (c < min) {
+			min = c;
+		}
+		if (d < min) {
+			min = d;
+		}
+		return min;
+	}
+	
+	public static int med2(int a, int b, int c) {
+		
+		if(a>=b) {
+			if(b>=c) {
+				return b;  // a = b = c , a > b > c , a=b > c, a>b=c
+			}else if(c>=a) {
+				return a;  // c=a=b, c>a>b, c=a>b, c>a=b
+			}else {
+				return c;
+			}
+		}else if(a > c) { // b는 a보다 작다는 가정하에 a < b
+			return a;
+		}else if(b > c) {
+			return c;
+		}else {
+			return b;
+		}
+	}
+	
+	public static void problem07() {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("1부터 n까지의 합을 구합니다");
+		System.out.print("n값 : ");
+		int n = sc.nextInt();
+		
+		int sum = 0;
+		int i = 1;
+		
+		while(i <= n) {
+		
+			sum += i;
+			i++;
+		}
+		
+		System.out.println("i의 값은 ? " + i + ", n은?? " + n + ", sum의 값은?" + sum );
+	
+		
+	}
+	
+	
+	public static int sumof(int a, int b) {
+		
+		int c  = (a+b)*((Math.abs(a-b)+1)/2);
+		
+		return c;
+	}
+	
+	public static void sumof2() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("a값 : ");
+		int a = sc.nextInt();
+		
+		int b;
+		
+		do {
+			System.out.print("b값 : ");
+			b = sc.nextInt();
+			
+			if(b<=a) {
+			System.out.println("a보다 큰 값을 입력하세요!!");
+			}
+		}while(b <= a);
+		
+		System.out.println("b-a는 " + (b-a) + "입니다.");
+		
+	}
+	
+	public static void sumof03() {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		
+		int count=0;
+		
+		while(a!=0) {
+			a/=10;
+			count++;
+			
+		}
+		System.out.println(count);
+	}
+	
+	public static void multi() {
+
+		for (int i = 1; i <= 9; i++) {
+
+			if (i == 1) {
+				System.out.print(" |");
+			}
+			System.out.printf("%3s", i);
+		}
+
+		System.out.println("\n----+------------------------");
+
+		for (int i = 1; i <= 9; i++) {
+			System.out.print(i + "|");
+			for (int j = 1; j <= 9; j++) {
+				System.out.printf("%3s", i * j);
+			}
+			System.out.println();
+		}
+	}
+
+	public static void multi2() {
+
+		for (int i = 1; i <= 9; i++) {
+
+			if (i == 1) {
+				System.out.print(" | ");
+			}
+			System.out.print(+i + " ");
+		}
+
+		System.out.println("\n----+-----------------");
+
+		for (int i = 1; i <= 9; i++) {
+			System.out.print(i + "| ");
+			for (int j = 1; j <= 9; j++) {
+				System.out.print(i + j + " ");
+			}
+			System.out.println();
+		}
+	}
+	public static void star01() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		
+		
+		for(int i = 1; i<=a ; i++) {
+			for(int j=1; j<=a;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void aaa() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		
+		for(int i = 1; i<=a ; i++) {
+			for(int j = 1; j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+	
+	public static void aaa2() {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		
+		for(int i = 0 ; i<a; i++) {
+			for(int j = a; j>i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void aaa3() {
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+
+		for (int i = 0; i < a; i++) {
+			
+			for(int j=0; j<i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = a; j > i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	public static void aaa4() {
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+
+		for (int i = 0; i < a; i++) {
+			
+			for(int j = a-1; j > i; j--) {
+				System.out.print(" ");
+			}
+			for (int j=0; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void spira() {
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+
+		for (int i = 0; i < a; i++) {
+
+			for (int j = a - 1; j > i; j--) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			for(int j = 0 ; j<i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void spira2() {
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+
+		for (int i = 1; i <= a; i++) {
+
+			for (int j = a - 1; j > i-1; j--) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j <= i-1; j++) {
+				System.out.print(i);
+			}
+			for(int j = 0 ; j<i-1; j++) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+
+}
---
+++ src/week1/YEOMCODING/run/Run.java
@@ -0,0 +1,25 @@
+package week1.YEOMCODING.run;
+
+import week1.YEOMCODING.Practice01;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		Practice01 a = new Practice01();
+		
+		
+//		System.out.println(a.Max4(1, 2, 3, 4));
+//		System.out.println(a.Min3(1, 3, 5));
+//		System.out.println(a.Min4(1, 2, 3, 4));
+//		System.out.println(a.med2(1, 3, 2));
+		//a.problem07();
+		//System.out.println(a.sumof(1, 10));
+		
+		//a.sumof03();
+		
+		a.spira2();
+		
+	}
+
+}
